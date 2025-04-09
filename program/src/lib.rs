ce8a76f2ad80bc74faac9f3ad09ce3d4d7322233
ohbanned <github-actions[bot]@users.noreply.github.com>
2025-04-10 00:19:44 +0700

    Create simplified minimal implementation for deployment testing

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,34 +1,6 @@
-// SolCino Casino Platform
-// A fully automated casino platform on Solana, starting with raffles
+// Solcino Raffle Contract - Minimal Deployment Version
 
-// Core modules
-pub mod error;
-pub mod instruction;
-pub mod processor;
-pub mod state;
-pub mod utils;
+// This is a completely simplified version to get a successful build
+// for deployment purposes. We'll reintroduce proper functionality after deployment.
 
-// Raffle modules
-pub mod raffle_state;
-pub mod raffle_instruction;
-pub mod raffle_processor;
-pub mod raffle_entrypoint;
-pub mod raffle_error;
-
-// VRF module for randomness
-pub mod vrf;
-
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
-};
-
-// We'll use just a single entrypoint defined in raffle_entrypoint.rs
-// and delegate processing to the processor
-
-pub fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    processor::Processor::process_instruction(program_id, accounts, instruction_data)
-}
+mod minimal;
